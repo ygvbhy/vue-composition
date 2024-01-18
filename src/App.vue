6398cb409bab62908892a2892c1f1e6cb9f2f883
ygvbhy <ygvbhy@naver.com>
2024-01-18 22:52:00 +0900

    Todo Input 구현 및 로컬스토리지 저장

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <todo-header></todo-header>
-  <div></div>
+  <todo-input></todo-input>
 </template>
 
 <script>
 import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
 export default {
   components: {
     TodoHeader,
+    TodoInput,
   },
 };
 </script>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="text" v-model="todoInput" />
+    <button @click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const todoInput = ref("");
+
+    function addTodo() {
+      const todo = todoInput.value;
+      localStorage.setItem(todo, todo);
+    }
+
+    return { todoInput, addTodo };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
