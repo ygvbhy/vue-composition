42a0836d5f7652a0d31356a62ab9a94fcf90bf95
ygvbhy <ygvbhy@naver.com>
2024-01-25 19:43:47 +0900

    기본 셋팅

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <todo-header></todo-header>
   <todo-input></todo-input>
+  <todo-list></todo-list>
 </template>
 
 <script>
 import TodoHeader from "./components/TodoHeader.vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
 export default {
   components: {
     TodoHeader,
     TodoInput,
+    TodoList,
   },
 };
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const items = ref(["애플", "삼성", "인프런"]);
+
+    return { items };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
