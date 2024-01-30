1419e77ef622cd498cf4ca722b6eb29bb6240568
ygvbhy <ygvbhy@naver.com>
2024-01-30 22:02:23 +0900

    할일 목록 컴포넌트 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <todo-header></todo-header>
   <todo-input></todo-input>
-  <todo-list></todo-list>
+  <todo-list :todoItems="todoItems"></todo-list>
 </template>
 
 <script>
 import TodoHeader from "./components/TodoHeader.vue";
 import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
+import { ref } from "vue";
 export default {
   components: {
     TodoHeader,
     TodoInput,
     TodoList,
+  },
+  setup() {
+    const todoItems = ref([]);
+
+    function fetchTodos() {
+      const result = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        const todoItem = localStorage.key(i);
+        // items.value.push(todoItem);
+        result.push(todoItem);
+      }
+      return result;
+    }
+
+    todoItems.value = fetchTodos();
+
+    return { todoItems };
   },
 };
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,27 +1,17 @@
 <template>
   <ul>
-    <li v-for="(item, index) in items" :key="index">
+    <li v-for="(item, index) in todoItems" :key="index">
       {{ item }}
     </li>
   </ul>
 </template>
 
 <script>
-import { ref } from "vue";
 export default {
-  setup() {
-    const items = ref([]);
-
-    function fetchTodos() {
-      for (let i = 0; i < localStorage.length; i++) {
-        const todoItem = localStorage.key(i);
-        items.value.push(todoItem);
-      }
-    }
-
-    fetchTodos();
-
-    return { items, fetchTodos };
+  props: {
+    todoItems: {
+      type: Array,
+    },
   },
 };
 </script>
